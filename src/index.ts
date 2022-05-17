1edc245fc37741e671bf5537f1abaab90943b063
eugbyte <eugenetham1994@gmail.com>
2022-05-17 13:08:34 +0800

    fix: typings

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,6 @@
 import singleQrVacCertHtml from "./emailTemplates/singleQrVacCert.html";
 import multiQrVacCertHtml from "./emailTemplates/multiQrVacCert.html";
 import multiQrRecCertHtml from "./emailTemplates/multiQrRecCert.html";
-export default { singleQrVacCertHtml, multiQrVacCertHtml, multiQrRecCertHtml };
+import newsletterVerify from "./emailTemplates/newsletterVerify.html";
+import newsletterWelcome from "./emailTemplates/newsletterWelcome.html";
+export default { singleQrVacCertHtml, multiQrVacCertHtml, multiQrRecCertHtml, newsletterVerify, newsletterWelcome };
